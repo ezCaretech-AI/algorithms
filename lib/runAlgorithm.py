468414b4353fc2fafa96b4e06d6770323a8be419
KyulKim <kyul3060@gmail.com>
2020-08-04 13:45:35 +0900

    # 0804 bubble sort code 개발패턴에 맞게 변경

--- lib/runAlgorithm.py
+++ lib/runAlgorithm.py
@@ -4,7 +4,7 @@ import random, time
 def runAlgorithm(funcs, funcsName, sampledata):
     n = len(sampledata)
     start_time=time.time()
-    funcs(sampledata)
+    funcs(sampledata) # return 없이 sampledata가 바뀜
     end_time=time.time() - start_time
 
     print('%s 실행 시간 (N=%d) : %0.3f'%(funcsName, n, end_time))
