221ddd5d2916252caf9bd7716a0dbb94554bf86e
KyulKim <kyul3060@gmail.com>
2020-08-24 16:35:06 +0900

    # 0824

--- lib/runAlgorithm.py
+++ lib/runAlgorithm.py
@@ -6,7 +6,7 @@ def runAlgorithm(funcs, funcsName, sampledata):
     start_time=time.time()
     funcs(sampledata) # return 없이 sampledata가 바뀜
     end_time=time.time() - start_time
-
+    
     print('%s 실행 시간 (N=%d) : %0.3f'%(funcsName, n, end_time))
     checkSort.checkSort(sampledata)
     print("")
