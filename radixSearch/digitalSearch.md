8a4d15df401629499f299e5944e4faa00ea4d630
shyun34 <shyun34@ezcaretech.com>
2020-10-08 11:34:12 +0900

    #201008 디지털 탐색 트리

---
+++ radixSearch/digitalSearch.md
@@ -0,0 +1,25 @@
+
+# Digital search tree란, 
+
+## 1. 삽입 원칙
+- 키의 최상위 비트부터 차례로 해당 비트가 0이면 왼쪽, 1이면 오른쪽으로 진행하여 외부 노드에 이르면 그 곳에 새로운 노드를 삽입
+
+ ![Alt text](/imgs/Digital/digital_search_ex.jpg)
+
+## 2. 탐색 원칙
+- 원소의 비트에 따라 tree를 내려가며 하나씩 비교
+
+
+## 3. 장단점
+- 장점 : 구현이 간단함
+- 단점 : tree를 내려가며 매번 키 값을 비교하기 때문에 탐색 키가 큰 경우에는 키 비교에 많은 시간이 소요되는 단점이 있음
+
+## 4. 응용분야
+- 네트워크 IP routing, packet classification, firewalls에서 사용됨
+
+## 5. 시간 복잡도
+> O(# bits in a key)
+
+
+
+http://inside.mines.edu/~dmehta/FDS_CPP/PPT/index.shtml
---
+++ radixSearch/digitalSearch.py
@@ -0,0 +1,76 @@
+class bitskey:
+    def __init__(self, x):
+        self.x = x
+    
+    def get(self):
+        return self.x
+
+
+    def bits(self, k ,j):
+        return (self.x >> k) & ~(~0<<j)
+
+class node:
+    def __init__(self, key):
+        self.key=bitskey(key)
+        self.left=None
+        self.right=None
+
+class Dict:
+    
+    maxb = 20
+    itemMin = bitskey(0)
+
+    z=node(itemMin)
+    head = node(itemMin)
+    head.left=z
+    head.right=z
+
+    def search(self, v):
+        
+        v=bitskey(v)
+        x=self.head.left
+        b=self.maxb
+        self.z.key=v
+        while v.get() != x.key.get():
+            b=b-1
+            if v.bits(b, 1):
+                x =x.right
+            else:
+                x=x.left
+        if x==self.z:
+            return -1
+        else:
+            return x.key.get()
+
+    def insert(self, v):
+        
+        v=bitskey(v)
+        b=self.maxb-1
+        x=self.head.left
+        p=self.head
+
+        while x.key.get() != self.z.key.get():
+            p=x
+            if v.bits(b,1):
+                x=x.right
+            else:
+                x=x.left
+            b-=1
+        x=node(self.itemMin)
+        x.key=v
+        x.left=self.z
+        x.right=self.z
+        if v.bits(b+1, 1):
+            p.right=x
+        else:
+            p.left=x
+
+    def run(self, key, searchValue):
+        d = Dict()
+        for i in range(len(key)):
+            d.insert(key[i])
+        
+        for i in range(len(key)):
+            result = d.search(searchValue)
+            if result == -1 or result != searchValue:
+                print('탐색오류')
